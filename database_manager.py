9694da53fc410b306795995471da7381196dbdb3
JoaoPedroPitarelo <jpspitarelo14@outlook.com>
2023-12-18 21:40:39 -0300

    Refatoração do código separando-o em pequenas funções e encapsulamento do BD

---
+++ database_manager.py
@@ -0,0 +1,44 @@
+import sqlite3
+
+
+class GerenciadorBD:
+    def __init__(self, db_name="database.db"):
+        self.conn = sqlite3.connect(db_name)
+        self.cursor = self.conn.cursor()
+
+    def close_connection(self):
+        self.conn.close()
+
+    def tarefas(self):
+        self.cursor.execute("SELECT * FROM TBTAREFA")
+        return self.cursor.fetchall()
+
+    def adicionar_tarefa(self, tarefa, data, obs):
+        self.cursor.execute("INSERT INTO TBTAREFA (tarefa, feita, data, nota) VALUES (?, 0, ?, ?)",
+                            (tarefa, data, obs))
+        self.conn.commit()
+
+    def indices(self):
+        self.cursor.execute("SELECT id_tarefa FROM TBTAREFA")
+        return self.cursor.fetchall()
+
+    def remover_tarefa(self, id_indice):
+        self.cursor.execute("DELETE FROM TBTAREFA WHERE id_tarefa = ?", (id_indice,))
+        self.conn.commit()
+
+    def alt_feita(self, id_indice):
+        self.cursor.execute("UPDATE TBTAREFA SET feita = ? WHERE id_tarefa = ?", (1, id_indice))
+        self.conn.commit()
+
+    def alt_nao_feita(self, id_indice):
+        self.cursor.execute("UPDATE TBTAREFA SET feita = ? WHERE id_tarefa = ?", (0, id_indice))
+        self.conn.commit()
+
+    def obsevacoes(self, task_id):
+        self.cursor.execute("SELECT nota FROM TBTAREFA WHERE id_tarefa = ?", (task_id,))
+        text_obsevacoes = self.cursor.fetchall()
+
+        if not text_obsevacoes[0][0].strip():
+            return None
+        else:
+            return text_obsevacoes[0][0]
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 """" Algoritmo de Lista To Do """
 import sqlite3
+from database_manager import GerenciadorBD
 import os
 
+# conexão com banco de dados
+db_manager = GerenciadorBD()
 
-conn = sqlite3.connect("database.db")  # conexão com banco de dados
-cursor = conn.cursor()
-
-RESET = '\033[0m'   # Cores
+# Cores e constantes
+RESET = '\033[0m'
 RED = '\033[31m'
 GREEN = '\033[32m'
 YELLOW = '\033[33m'
@@ -17,73 +18,92 @@ WHITE = '\033[37m'
 NEGRITO = '\033[1m'
 
 
-def main():  # Função main
-    clear_terminal()
-    """Coleta dos dados do DB e retorna uma lista de tarefas"""
-    cursor.execute("SELECT * FROM TBTAREFA")  # Coletar dados do BD
-    lista_tarefas = cursor.fetchall()  # Captura dos dados
+def clear_terminal():
+    if os.name == 'nt':  # Windows
+        os.system('cls')
+    elif os.name == 'posix':
+        os.system('clear')  # Linux
 
-    print(NEGRITO, " ")  # Logo
-    print(
-        "                      ",                   GREEN + "_____                   _             _ \n" +
-                WHITE + "     /////|           " + GREEN + "|_   _|__ _ __ _ __ ___ (_)_ __   __ _| |\n" +
-                WHITE + "    ///// |    (\       " + GREEN + "| |/ _ \ '__| '_ ` _ \| | '_ \ / _` | |\n" +
-                WHITE + "   |~~~|  |    \ \      " + GREEN + "| |  __/ |  | | | | | | | | | | (_| | |\n" +
-                WHITE + "   |===|  |     \ \     " + GREEN + "|_|\___|_|  |_| |_| |_|_|_| |_|\__,_|_|\n" +
-                WHITE + "   |T  |  |     / '|   " + MAGENTA + "         _____     ____   ___  _     ___    _____\n" +
-                WHITE + "   | D |  |     \ '/  " + MAGENTA+  "         |_   _|__ |  _ \ / _ \| |   |_ _|__|_   _|\n" +
-                WHITE + "   |  L| /        \      " + MAGENTA + "        | |/ _ \| | | | | | | |    | |/ __|| |  \n" +
-                WHITE + "   |===|/         ==).   " + MAGENTA + "        | | (_) | |_| | |_| | |___ | |\__ \| | \n" +
-                WHITE + "   '---'         (__)   " + MAGENTA + "         |_|\___/|____/ \___/|_____|___|___/|_| \n" + RESET
+
+#  Funções da parte "Home" do algoritmo
+def logo():
+    print(NEGRITO, "                     ", GREEN + "_____                   _             _ \n" +
+        WHITE + "     /////|           " + GREEN + "|_   _|__ _ __ _ __ ___ (_)_ __   __ _| |\n" +
+        WHITE + "    ///// |    (\       " + GREEN + "| |/ _ \ '__| '_ ` _ \| | '_ \ / _` | |\n" +
+        WHITE + "   |~~~|  |    \ \      " + GREEN + "| |  __/ |  | | | | | | | | | | (_| | |\n" +
+        WHITE + "   |===|  |     \ \     " + GREEN + "|_|\___|_|  |_| |_| |_|_|_| |_|\__,_|_|\n" +
+        WHITE + "   |T  |  |     / '|   " + MAGENTA + "         _____     ____   ___  _     ___    _____\n" +
+        WHITE + "   | D |  |     \ '/  " + MAGENTA+  "         |_   _|__ |  _ \ / _ \| |   |_ _|__|_   _|\n" +
+        WHITE + "   |  L| /        \      " + MAGENTA + "        | |/ _ \| | | | | | | |    | |/ __|| |  \n" +
+        WHITE + "   |===|/         ==).   " + MAGENTA + "        | | (_) | |_| | |_| | |___ | |\__ \| | \n" +
+        WHITE + "   '---'         (__)   " + MAGENTA + "         |_|\___/|____/ \___/|_____|___|___/|_| \n" + RESET
     )
 
-    if not lista_tarefas:  # Menu de tarefas
+
+def exibir_tarefas():
+    lista_tarefas = db_manager.tarefas()
+
+    if not lista_tarefas:
         print(NEGRITO + GREEN + "\n Não há nada para fazer :)" + RESET)
     else:
-        print("_" * 17, "\n")
+        print("_" * 24, "\n")
         for i, tarefa in enumerate(lista_tarefas):
-            print("  FEITA     |" if tarefa[2] == 1 else "  NÃO FEITA |", "ID: ", tarefa[0], "| Data: ", tarefa[3],
+            print(GREEN + " \u2713 " + RESET if tarefa[2] == 1 else RED + " \u2718 " + RESET, "|",
+                  "ID: ", tarefa[0],
+                  "| Data: ", tarefa[3],
                   "| Tarefa: ", tarefa[1])
+        print("_" * 24, "\n")
 
+
+def entrada_opcao():
     print("\nOPÇÕES: ")
-    print("[A]dicionar Tarefa  [N]ão Feita\n"  # Opções de entrada do Usuário
-          "[R]emover Tarefa    [F]eita")
+    print("     [A]dicionar Tarefa  [N]ão Feita  [O]BS tarefas\n"  # Opções de entrada do Usuário
+          "     [R]emover Tarefa    [F]eita      [S]air")
 
-    while True:  # Entrada opção
+    while True:
         entrada_opcao = input('\n Escolha: ').upper().strip()
 
-        if entrada_opcao.isalpha() and entrada_opcao.upper() in ["A", "R", "F", "N"]:
+        if entrada_opcao.isalpha() and entrada_opcao.upper() in ["A", "R", "F", "N", "O", "S"]:
             break
         else:
-            print("\n Entrada INVALIDA >(")
-            # entrada_opcao = input('\n Escolha: ').upper().strip()  # Entrada opção
+            print(RED, "\n Entrada INVALIDA >(", RESET)
 
     if entrada_opcao == "A":  # "Switch case" da entrada
         adicionar_tarefa()
         clear_terminal()
-        main()
     elif entrada_opcao == "R":
         remover_tarefa()
-        # clear_terminal()
-        main()
+        clear_terminal()
     elif entrada_opcao == "N":
         alt_nao_feita()
-        main()
+        clear_terminal()
     elif entrada_opcao == "F":
         alt_feita()
-        main()
-    else:
-        # clear_terminal()
-        print("\n Nenhuma das opções Selecionadas :(")
-        main()
+        clear_terminal()
+    elif entrada_opcao == "O":
+        acessar_observacoes()
+        clear_terminal()
+    elif entrada_opcao == "S":
+        return entrada_opcao
 
 
-def adicionar_tarefa():  # Adicionar nova tarefa à lista
-    nova_tarefa = input("\n Nova Tarefa: ")
+#   Função Principal(main)
+def main():  # Função main
+    while True:
+        clear_terminal()
+        logo()
+        exibir_tarefas()
 
-    while True:  # Entrada da data
+        if entrada_opcao() == "S":
+            break
 
-        dia_entrada = input("  Dia: ")
+
+#  Funções das opções do algoritmo (adicionar, remover, marcar como feita...)
+
+#  Validação da data
+def validacao_data():
+    while True:
+        dia_entrada = input("\n  Dia: ")
         mes_entrada = input("    Mês: ")
         ano_entrada = input("      Ano: ")
 
@@ -93,111 +113,83 @@ def adicionar_tarefa():  # Adicionar nova tarefa à lista
             ano_entrada = int(ano_entrada)
 
             if 0 <= dia_entrada >= 30 and mes_entrada in [2]:  # Verificação dos dias com seus respectivos meses
-                print("\n Data INVALIDA")
+                print(RED, "\n Data INVALIDA", RESET)
             elif 0 <= dia_entrada >= 31 and mes_entrada in [4, 6, 9, 11]:
-                print("\n Data INVALIDA")
+                print(RED, "\n Data INVALIDA", RESET)
             elif 1 <= dia_entrada <= 31 and 1 <= mes_entrada <= 12 and 2000 <= ano_entrada <= 3000:
                 break
             else:
-                print("\n Data INVALIDA")
+                print(RED, "\n Data INVALIDA", RESET)
         except ValueError:
-            print("\n Data INVALIDA")
+            print(RED, "\n Data INVALIDA", RESET)
 
-    if dia_entrada in range(0, 9) or mes_entrada in range(0, 9):  # Formatação da data. Para não ficar Ex: 1/1/2024
-        data_formatada = f"0{dia_entrada}/0{mes_entrada}/{ano_entrada}"
+    return dia_entrada, mes_entrada, ano_entrada
+
+
+def adicionar_tarefa():
+    print('\n "Faça em breves palavras"')
+    nova_tarefa = input("   Nova Tarefa: ")
+    entrada_obsevacao = input("      Obs da tarefa: ")
+
+    data_validada = validacao_data()
+
+    if data_validada[0] in range(0, 9) or data_validada[1] in range(0, 9):
+        data_formatada = f"0{data_validada[0]}/0{data_validada[1]}/{data_validada[2]}"
     else:
-        data_formatada = f"{dia_entrada}/{mes_entrada}/{ano_entrada}"
+        data_formatada = f"{data_validada[0]}/{data_validada[1]}/{data_validada[2]}"
 
-    cursor.execute("INSERT INTO TBTAREFA (tarefa, feita, data) VALUES (?, 0, ?)", (nova_tarefa, data_formatada))
-    conn.commit()
+    db_manager.adicionar_tarefa(nova_tarefa, data_formatada, entrada_obsevacao)
+
+
+#  Validação dos índices
+def validacao_indice():
+    lista_de_tuplas_indices = db_manager.indices()
+    tupla_indice = ()
+
+    for tupla in lista_de_tuplas_indices:
+        tupla_indice += tupla
+
+    entrada_indice = ''
+    while True:
+        if tupla_indice == ():
+            break
+
+        entrada_indice = input("\n   Índice: ").strip()
+
+        try:
+            entrada_indice = int(entrada_indice)
+            if entrada_indice in tupla_indice:
+                print(GREEN, "Encontrado", RESET)
+                break
+            else:
+                print(RED, "\n Índice não encontrado", RESET)
+        except ValueError:
+            print(RED, "\n Índice INVALIDO ", RESET)
+
+    return entrada_indice
 
 
 def remover_tarefa():  # Remover tarefa
-    cursor.execute("SELECT id_tarefa FROM TBTAREFA")  # Coleta dos índices no BD
-    lista_de_tuplas_indices = (cursor.fetchall())  # Lista de tuplas [(0,), (1,), (2,)]
-    tupla_indice = ()
-
-    for tupla in lista_de_tuplas_indices:  # Desempacotando a lista de tuplas em somente uma tupla
-        tupla_indice += tupla
-
-    while True:
-        if tupla_indice == ():  # Caso a lista esteja vazia
-            break
-
-        entrada_indice = input("\n Índice: ").strip()
-
-        try:
-            entrada_indice = int(entrada_indice)
-            if entrada_indice in tupla_indice:
-                print("Encontrado")
-                cursor.execute(f"DELETE FROM TBTAREFA WHERE id_tarefa = {entrada_indice};")
-                conn.commit()
-                break
-            else:
-                print("\n Índice não encontrado")
-        except ValueError:
-            print("\n Índice INVALIDO ")
+    db_manager.remover_tarefa(validacao_indice())
 
 
 def alt_feita():  # Mudar para feita a tarefa
-    cursor.execute("SELECT id_tarefa FROM TBTAREFA")  # Coleta dos índices no BD
-    lista_de_tuplas_indices = (cursor.fetchall())  # Lista de tuplas [(0,), (1,), (2,)]
-    tupla_indice = ()
-
-    for tupla in lista_de_tuplas_indices:  # Desempacotando a lista de tuplas em somente uma tupla
-        tupla_indice += tupla
-
-    while True:
-        if tupla_indice == ():  # Caso a lista esteja vazia
-            break
-
-        entrada_indice = input("\n Índice: ").strip()
-
-        try:
-            entrada_indice = int(entrada_indice)
-            if entrada_indice in tupla_indice:
-                print("Encontrado")
-                cursor.execute("UPDATE TBTAREFA SET feita = ? WHERE id_tarefa = ?", (1, entrada_indice))
-                conn.commit()
-                break
-            else:
-                print("\n Índice não encontrado")
-        except ValueError:
-            print("\n Índice INVALIDO >(")
+    db_manager.alt_feita(validacao_indice())
 
 
 def alt_nao_feita():  # Mudar para não feita a tarefa
-    cursor.execute("SELECT id_tarefa FROM TBTAREFA")  # Coleta dos índices no BD
-    lista_de_tuplas_indices = (cursor.fetchall())  # Lista de tuplas [(0,), (1,), (2,)]
-    tupla_indice = ()
-
-    for tupla in lista_de_tuplas_indices:  # Desempacotando a lista de tuplas em somente uma tupla
-        tupla_indice += tupla
-
-    while True:
-        if tupla_indice == ():  # Caso a lista esteja vazia
-            break
-
-        entrada_indice = input("\n Índice: ").strip()
-
-        try:
-            entrada_indice = int(entrada_indice)
-            if entrada_indice in tupla_indice:
-                print("Encontrado")
-                cursor.execute("UPDATE TBTAREFA SET feita = ? WHERE id_tarefa = ?", (0, entrada_indice))
-                conn.commit()
-                break
-            else:
-                print("\n Índice não encontrado")
-        except ValueError:
-            print("\n Índice INVALIDO >(")
+    db_manager.alt_nao_feita(validacao_indice())
 
 
-def clear_terminal():  # Função para limpar o terminal
-    if os.name == 'nt':  # Caso seja Windows
-        os.system('cls')
-    elif os.name == 'posix':
-        os.system('clear')  # Caso seja sistemas Linux
+def acessar_observacoes(): # Observações
+    if db_manager.obsevacoes(validacao_indice()) is None:
+        print(RED, NEGRITO, "\nNão há observação para essa tarefa!", RESET)
+        pause = input("\nPressione ENTER para continuar")
+    else:
+        print(f'\n {NEGRITO}   " {db_manager.obsevacoes(validacao_indice())} "{RESET}')
+        pause = input("\nPressione ENTER para continuar")
 
 
-main()
+if __name__ == "__main__":
+    main()
+
