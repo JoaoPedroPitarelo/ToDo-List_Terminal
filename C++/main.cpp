4a9e12d10aa0f078232636b92cc1e7fd226f37dd
João Pedro Pitarelo <JoaoPedroPitarelo>
2023-12-27 21:33:22 -0300

    Teste no windows

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <mysql_driver.h>
+#include <mysql_connection.h>
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
 using namespace std;
+
 
 
 int main() {
 
+    sql::mysql::MySQL_Driver *driver;
+    sql::Connection *con;
+
+    driver = sql::mysql::get_mysql_driver_instance();
+    con = driver->connect("tcp://127.0.0.1:3306", "root", "");
+    con->setSchema("seu_banco_de_dados");
+
+
     cout << "sim";
+    return 0;
 }
 
