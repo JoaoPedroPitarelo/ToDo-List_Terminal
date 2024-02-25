7c0b0282d135f2edf0110e7e4acd4913fedec319
João Pedro Salmazo Pitarelo <142443774+JoaoPedroPitarelo@users.noreply.github.com>
2024-02-25 00:01:34 -0300

    Criação do DataBaseManager

---
+++ ToDo-List_TerminalOO/DataBaseManager.py
@@ -0,0 +1,65 @@
+import sqlite3
+
+class DataBaseManager:
+    def __init__(self, db_name="database.db"):
+        self.conn = sqlite3.connect(db_name)
+        self.cursor = self.conn.cursor()
+    
+    def create_tables(self):
+        self.cursor.execute("create table if not exists tbcategory(category_name text primary key)")
+        self.cursor.execute("create table if not exists tbtask(id_task INTEGER primary key,"
+                            " task_name text not null,"
+                            " state integer not null,"
+                            " date date not null,"
+                            " task_description text not null,"
+                            " priority int not null,"
+                            " category text,"
+                            " foreign key (category) references tbcategory(category_name))")
+        self.conn.commit()
+
+    def close_connection(self):
+        self.conn.close()
+
+
+    def get_tasks(self):
+        self.cursor.execute("SELECT * FROM tbtask ORDER BY id_task")
+        return self.cursor.fetchall()
+    
+    def get_indexes(self):
+        self.cursor.execute("SELECT id_task FROM tbtask")
+        return self.cursor.fetchall()
+
+    def get_categories(self):
+        self.cursor.execute("SELECT category_name FROM tbcategory")
+        return self.cursor.fetchall()
+
+    def get_description(self, id_task):
+        self.cursor.execute("SELECT task_description FROM tbtask WHERE id_task = ?", (id_task,))
+        return self.cursor.fetchall()
+    
+
+    def add_task(self, task_name, state, date, description, priority):
+        self.cursor.execute("INSERT INTO tbtask (task_name, state, date, priority, description, category_name) VALUES (?, 0, ?, ?, ?, ?)",
+                            (task_name, state, date, description, priority))
+        self.conn.commit()
+    
+    def remove_task(self, id_task):
+        self.cursor.execute("DELETE FROM tbtask WHERE id_task = ?", (id_task))
+        self.conn.commit()
+
+    def add_category(self, category_name):
+        self.cursor.execute("INSERT INTO tbcategory (category_name) VALUES (?)", (category_name,))
+        self.conn.commit()
+        
+    def remove_category(self, category_name):
+        self.cursor.execute("DELETE FROM tbtask WHERE category_name = ?", (category_name,))
+        self.conn.commit()
+    
+
+    def modify_status_to_done(self, id_task):
+        self.cursor.execute("UPDATE tbtask SET state = ? WHERE id_task = ?", (1, id_task))
+        self.conn.commit()
+
+    def modify_status_to_not_done(self, id_task) :
+        self.cursor.execute("UPDATE tbtask SET state = ? WHERE id_task = ?", (0, id_task))
+
--- ToDo-List_TerminalOO/Task.py
+++ ToDo-List_TerminalOO/Task.py
@@ -1,17 +1,22 @@
 class Task:
-    def __init__(self, state, index, date, priority, name):
-        self.__state = state
+    def __init__(self, state, index, date, priority, name, description):
         self.__index = index
+        self.__name = name
+        self.__state = state
         self.__date = date
         self.__priority = priority
-        self.__name = name
+        self.__description = description
+        
        
     # Getters 
-    def get_state(self):
-        return self.__state
-    
     def get_index(self):
         return self.__index
+    
+    def get_name(self):
+        return self.__name
+    
+    def get_state(self):
+        return self.__state
     
     def get_date(self):
         return self.__date
@@ -19,8 +24,13 @@ class Task:
     def get_priority(self):
         return self.__priority
     
-    def get_name(self):
-        return self.__name
-    
-    
+    def get_description(self):
+        return self.__description
+        
+
+    def set_task(self, state , index, date, priority, name):
+        
+            
+
+
     
